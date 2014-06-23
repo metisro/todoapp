4d0d15ae2225531449ed1576d4feb8318482cabb
metisro <flavius.gheorghe@gmail.com>
2014-06-23 16:45:21 +0200

    User Login created

--- app/routes.php
+++ app/routes.php
@@ -2,5 +2,5 @@
 
 Route::get('/', array('as' => 'home', 'uses' => 'HomeController@getIndex'));
 
-Route::get('/login', array('as' => 'login', 'uses' => 'AuthController@getLogin'));
-Route::post('login', array('uses' => 'AuthController@postLogin'));
+Route::get('/login', array('as' => 'login', 'uses' => 'AuthController@getLogin'))->before('guest');
+Route::post('login', array('uses' => 'AuthController@postLogin'))->before('csrf');
